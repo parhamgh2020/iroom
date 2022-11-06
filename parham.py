9eaea61c7d03e408d37b82180bf7ca3466eb1ccc
pgh <you@example.com>
2022-11-06 15:25:10 +0330

    add readme

--- parham.py
+++ parham.py
@@ -2,16 +2,22 @@ from fastapi import FastAPI
 from auth import authentication
 from router import user, room
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import auth
 
 app = FastAPI()
 
 app.add_middleware(
-  CORSMiddleware,
-  allow_credentials = True,
-  allow_methods = ["*"],
-  allow_headers = ['*']
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=['*']
 )
 
 app.include_router(authentication.router)
 app.include_router(user.router)
 app.include_router(room.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
